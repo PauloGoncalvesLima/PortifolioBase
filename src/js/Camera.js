53666a72c86debfdf05815b1517d01cf1e23b9a0
gabrieldamotta <gabrieldamotta99@gmail.com>
2020-07-02 16:05:05 -0300

    preliminary camera complete
    Co-authored-by: Paulo Gonçalves Lima <paulogoncalves436@gmail.com>

--- src/js/Camera.js
+++ src/js/Camera.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-// TODO: GSAP
 
 export default class Camera {
 
@@ -14,33 +13,43 @@ export default class Camera {
 
     // set up
     this.container = new THREE.Object3D();
-    this.container.matrixAutoUpdate = false; // ???
+    this.container.matrixAutoUpdate = false; // save updates to resize
 
     this.target = new THREE.Vector3();
+    this.direction = new THREE.Vector3();
     this.targetEased = new THREE.Vector3();
     this.easing = 0.15;
 
     // debug folder
     if (this.debug) {
-      // this.debugFolder = this.debug.addFolder('camera');
+      this.debugFolder = this.debug.addFolder('camera');
     }
 
     this.setAngle();
     this.setInstance();
     this.setZoom();
-    this.setPan();
     this.setOrbitControls();
   }
 
   setAngle() {
-    // TODO: Brainstorm a way to get the camera angle relative to the car
     // set up
     this.angle = {};
+    this.angle.value = new THREE.Vector3();
+    this.angle.height = 0.5;
+    this.angle.lerpAlpha = 0.125;
 
-    // each possible angle
-    this.angle.items = {
-      default: new THREE.Vector3()
+    // debug
+    if(this.debug)
+    {
+      this.debugFolder.add(this.angle, 'lerpAlpha').step(0.0001).min(0).max(1).name('lerpAlpha');
+      this.debugFolder.add(this.angle, 'height').step(0.0001).min(0).max(100).name('height');
     }
+
+    // update event
+    this.time.on('update', () => {
+      let targetAngle = this.direction.clone().negate().add(new THREE.Vector3(0, 0, this.angle.height));
+      this.angle.value.lerp(targetAngle, this.angle.lerpAlpha);
+    });
   }
 
   setInstance() {
@@ -48,6 +57,7 @@ export default class Camera {
     this.instance = new THREE.PerspectiveCamera(70, this.sizes.viewport.width / this.sizes.viewport.height, 1, 80);
     this.instance.up.set(0, 0, 1);
     this.instance.position.copy(this.angle.value);
+    this.instance.distance = 8;
     this.instance.lookAt(new THREE.Vector3());
     this.container.add(this.instance);
 
@@ -63,18 +73,17 @@ export default class Camera {
       // out of debug camera
       if (!this.orbitControls.enabled) {
         // TODO: Easing
-        
+        this.instance.position.copy(this.target).add(this.angle.value.clone().multiplyScalar(this.instance.distance));
+        this.instance.lookAt(this.target);
       }
     });
   }
-
-  setZoom() {
-
-  }
-
-  setPan() {
-
-  }
+  
+  // TODO: Zoom mouse wheel
+  // setZoom() {
+  //   this.zoom = {};
+  //   this.zoom.distance = 0.8;
+  // }
 
   setOrbitControls() {
     // set up
@@ -85,7 +94,7 @@ export default class Camera {
 
     // debug (enable/disable)
     if (this.debug) {
-      // this.debugFolder.add(this.orbitControls, 'enabled').name('orbitControlsEnabled');
+      this.debugFolder.add(this.orbitControls, 'enabled').name('orbitControlsEnabled');
     }
   }
 }

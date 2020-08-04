2339f70bc10a39057646b387d0a91bcb3a95fa86
gabrieldamotta <gabrieldamotta99@gmail.com>
2020-08-04 19:46:18 -0300

    started configuring application.js and implemented app entrypoint index.js; minor compatibility changes

---
+++ src/js/Application.js
@@ -0,0 +1,105 @@
+import * as THREE from 'three';
+
+// utils
+import Sizes from './utils/Sizes.js';
+import Time from './utils/Time.js';
+
+import Resources from './Resources.js';
+import Camera from './Camera.js';
+
+import World from './World/World.js';
+
+export default class Application {
+    /**
+     * Constructor
+     */
+    constructor(_options) {
+        // options
+        this.$canvas = _options.$canvas;
+
+        // set up
+        this.time = new Time();
+        this.sizes = new Sizes();
+        this.resources = new Resources();
+
+        // TODO: finish setup functions
+        this.setConfig();
+        this.setDebug();
+        this.setRenderer();
+        this.setCamera();
+        // this.setPasses();
+        this.setWorld();
+        // this.setTitle();
+    }
+
+    // TODO:
+    setConfig() {
+
+    }
+
+    // TODO:
+    setDebug() {
+
+    }
+
+    /**
+     * Initializes the THREE scene and renderer to
+     * the canvas given in constructor options.
+     */
+    setRenderer() {
+        // set up scene
+        this.scene = new THREE.Scene();
+
+        // set up renderer
+        this.renderer = new THREE.WebGLRenderer ({
+            canvas: this.$canvas,
+            alpha = true
+        });
+        this.renderer.setClearColor("black", 1);
+        this.renderer.setPixelRatio(2); // improves anti-aliasing
+        this.renderer.setSize(this.sizes.viewport.width, this.sizes.viewport.height);
+        // this.renderer.physicallyCorrectLights = true;
+        this.renderer.gammaFactor = 2.2;
+        this.renderer.gammaOutPut = true;
+        this.renderer.autoClear = false;
+
+        // adjust aspect on resize event
+        this.sizes.on('resize', () => {
+            this.renderer.setSize(this.sizes.viewport.width, this.sizes.viewport.height);
+        });
+    }
+
+    /**
+     * Initializes camera and updates its
+     * movement through the car position and
+     * facing direction.
+     */
+    setCamera() {
+        // set up camera
+        this.camera = new Camera({
+            time: this.time,
+            sizes: this.sizes,
+            renderer: this.renderer,
+            debug: this.debug,
+            config: this.config
+        });
+
+        // add to scene
+        this.scene.add(this.camera.container);
+
+        // update camera position per frame
+        this.time.on('update', () => {
+            if (this.world && this.world.car) { // if car is on the scene
+                this.camera.target.x = this.world.car.chassis.object.position.x;
+                this.camera.target.y = this.world.car.chassis.object.position.y;
+                this.camera.direction.x = this.world.car.movement.direction.x;
+                this.camera.direction.y = this.world.car.movement.direction.y;
+            }
+        });
+    }
+
+    // TODO:
+    setWorld() {
+
+    }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,90 +1,9 @@
-import "../css/style.css"
-import * as THREE from 'three';
+import '../css/style.css';
+import Application from './Application.js';
 
-import Resources from './Resources';
-
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-// RENDERER!!!!
-var renderer = new THREE.WebGLRenderer();
-renderer.setClearColor("white", 1);
-renderer.setPixelRatio(2);
-renderer.physicallyCorrectLights = true;
-renderer.outputEncoding = THREE.sRGBEncoding;
-// renderer.autoClear = false;
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-window.addEventListener('resize', () => {
-  var width = window.innerWidth;
-  var height = window.innerHeight;
-  renderer.setSize(width, height);
-  camera.aspect = width / height;
-  camera.updateProjectionMatrix();
+/**
+ * Entry point for the entire app
+ */
+window.application = new Application ({
+    $canvas: document.querySelector('.js-canvas')
 });
-
-var controls = new OrbitControls(camera, renderer.domElement);
-
-camera.position.z = 3;
-
-const resources = new Resources();
-// add objects
-resources.on('ready', () => {
-  console.log('scene is ready to load');
-  // add car
-  scene.add(resources.items['car'].scene);
-});
-
-// lighting
-var ambientLight = new THREE.AmbientLight("white", 1);
-scene.add(ambientLight);
-
-var pointLight = new THREE.PointLight("white", 75, 100);
-pointLight.position.set(3, 3, 3)
-
-var targetObject = new THREE.Object3D();
-scene.add(targetObject);
-
-var directionalLight = new THREE.DirectionalLight("white", 15);
-directionalLight.position.set(0,7,10);7
-
-directionalLight.rotation.set(0, 90 * (Math.PI/180), 0);
-targetObject.position.set(0,5, -3);
-directionalLight.target = targetObject;
-
-scene.add(directionalLight);
-
-var spotLight = new THREE.SpotLight("white", 25);
-spotLight.position.set(0, 3, 0);
-
-// game logic
-let update = () => {
-  // cube.rotation.x += 0.01;
-  // cube.rotation.y += 0.005;
-
-  // var time = Date.now() * 0.0005;
-
-  // pointLight.position.x = Math.sin(time * 0.7) * 3;
-  // pointLight.position.y = Math.sin(time * 0.5) * 4;
-  // pointLight.position.z = Math.sin(time * 0.3) * 3;
-}
-
-// draw scene
-let render = () => {
-  renderer.render(scene, camera);
-}
-
-// run game loop (update, render, repeat)
-let gameLoop = () => {
-  requestAnimationFrame(gameLoop); // run every frame
-
-  controls.update();
-  update();
-  render();
-}
-
-// run
-gameLoop();
